fd1e932c53ef90452f3e9d27d6f0dc7979666faa
Snail <>
2026-08-20 02:10:32 +0800

    Rename chatbot, add greeting and exit.

---
+++ src/main/java/Bean.java
@@ -0,0 +1,29 @@
+public class Bean {
+    public static void main(String[] args) {
+                    String banner =
+                        "       ▄▄▄                    \n" +
+                        "      ██▀▀█▄                  \n" +
+                        "      ██ ▄█▀             ▄    \n" +
+                        "      ██▀▀█▄ ▄█▀█▄ ▄▀▀█▄ ████▄\n" +
+                        "    ▄ ██  ▄█ ██▄█▀ ▄█▀██ ██ ██\n" +
+                        "    ▀██████▀▄▀█▄▄▄▄▀█▄██▄██ ▀█\n";
+                           
+                           
+        System.out.println("----------------------------------");
+        System.out.println(banner);
+        System.out.println("----------------------------------");
+        String intro = "Hello! I'm Bean.";
+        String message = "What can I do for you today?";
+        String goodbye = "Bye bye! Hope to see you again soon!";
+        System.out.println();
+        System.out.println(intro + "\n");
+        System.out.println(message);
+        System.out.println();
+        System.out.println("----------------------------------");
+        System.out.println();
+        System.out.println(goodbye);
+        System.out.println();
+        System.out.println("----------------------------------");
+
+    }
+}
